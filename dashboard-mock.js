cd28f23289a72b43ff891a4999e6bb0fcb3cc0b6
Jason Walsh <j@wal.sh>
2015-07-16 18:50:03 -0400

    Mocking single user talk events

---
+++ dashboard-mock.js
@@ -0,0 +1,28 @@
+console.log('dashboard-mock');
+
+var events = setInterval(function() {
+    var event = {
+        user: 'user1',
+        date: (new Date()),
+        type: 'talk'
+    };
+    var activity = document.getElementById('activity');
+    var entry = document.createElement('tr');
+    var data = '<td>' + event.user + '</td><td>' + event.date + '</td>';
+    entry.innerHTML = data;
+    document.getElementById('activity').appendChild(entry);
+}, 10000 * Math.random());
+
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['User', 'Talking'],
+        ['user1', 10]]);
+
+    var options = {
+        title: 'Talk Time by User',
+        legend: { position: 'none' }
+    };
+};
